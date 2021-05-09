cdea4de03d3b12e0d3e6634d05b384689b45ac76
lab-1 <sam-jia@sfu.ca>
2021-05-09 01:09:56 +0000

    finished forgot password

--- project/__init__.py
+++ project/__init__.py
@@ -5,7 +5,6 @@ from flask_mail import Mail
 import os
 from flask_bcrypt import Bcrypt
 
-
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 mail = Mail()
@@ -19,8 +18,8 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
     app.config['MAIL_PORT'] = 587 #587 is the port for TLS: Transport Layer Security is a security protocol that encrypts email to protect its privacy
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = ekokeys2021@gmail.com
-    app.config['MAIL_PASSWORD'] = bigboobz
+    app.config['MAIL_USERNAME'] = 'ekokeys2021@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'bigboobz'
     mail.init_app(app)
 
     db.init_app(app)
